98b39da5c94b7ff45a26355e9dd29998e64875e0
bancerk <cbsezen@gmail.com>
2023-01-29 15:55:38 +0300

    rename

--- python-learning/7-while-loop.py
+++ python-learning/7-while-loop.py
@@ -4,4 +4,4 @@
 i=0
 while i<4: 
     print(i)
-    i+=1
+    i += 1
---
+++ python-learning/8-loop-control-statements1.py
@@ -0,0 +1,9 @@
+#loop control statements:
+# break,pass,continue
+
+for x in "Edureka!":
+    if x == "r":
+        break
+    print(x)
+print("loop has ended")
+
