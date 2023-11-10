310f57e794464a42724b058f3b0d26b7c08f46d3
bancerk <cbsezen@gmail.com>
2023-11-10 15:09:15 +0300

    loading..

---
+++ python/4-variables.py
@@ -0,0 +1,21 @@
+my_var = "Hello World"
+
+print(my_var)
+
+# type(my_var) in python shell will show if string/variable/integer
+
+# print("my_var")
+# this is false use. brackets indicate a string not a variable.
+
+my_var_1 = "learn "
+my_var_2 = "python"
+
+print(my_var_1 + my_var_2)
+
+var1 = 10
+var2 = 56
+
+print(var1 + var2)
+
+my_result = var1 + var2
+print(my_result)
