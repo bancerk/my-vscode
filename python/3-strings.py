7a0ae7a238aaaff37572d48216d4ea02b170ba99
bancerk <cbsezen@gmail.com>
2023-11-10 12:58:07 +0300

    re python

---
+++ python/3-strings.py
@@ -0,0 +1,21 @@
+print("Hello")
+
+print('Hello')
+
+# notice same output
+
+
+
+# print("My name is "Jay", nice to meet you.")
+
+# quotes within string break syntax
+
+print("My name is \"Jay\", nice to meet you.")
+
+# \ ignores next character
+
+print('My name is "Jay", nice to meet you.')
+
+# or use single quotes
+
+print('Sue said "I don\'t like pizza."')
