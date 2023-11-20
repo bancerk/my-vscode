d27a9fbdf302d5f0f917eedf150bdbd1f5a33864
bancerk <cbsezen@gmail.com>
2023-11-20 15:16:01 +0300

    testing..

---
+++ python/self-test-2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+# simple calculator
+
+num_1 = input("Please enter your first number:")
+
+num_1 = float(num_1)
+
+num_2 = input("Please enter your second number:")
+
+num_2 = float(num_2)
+
+my_op = input("Please enter your desired operation:")
+
+if(my_op == "+"):
+    print(num_1 + num_2)
+elif(my_op == "-"):
+    print(num_1 - num_2)
+elif(my_op == "*"):
+    print(num_1 * num_2)
+elif(my_op == "/"):
+    print(num_1 / num_2)
+elif(my_op == "%"):
+    print(num_1 % num_2)
+else:
+    print("Invalid operation")
---
+++ python/self-test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+# Celcius to Fahrenheit converter
+
+# take user input
+
+# print("Please put in degree in Celcius to convert to Fahrenheit:")
+
+C = input("Please put in degree in Celcius to convert to Fahrenheit: ")
+
+C = int(C)
+
+F = (C*9/5+32)
+
+F = int(F)
+
+# print(C)
+
+# print("Celcius degrees equals to")
+
+# print(F)
+
+# print("Fahrenheit degrees")
+
+print(str(C) + " Celcius degrees equals to "+ str(F) +" Fahrenheit degrees")
